9f0333418b0eebc42a87f032346f86d5125b4880
akatsukinoyami <hirokod3@gmail.com>
2023-10-03 23:48:17 +0300

    fix issue with navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import Search from "./Search.svelte";
+
+  let is_active = false;
 </script>
 
 <nav class="navbar is-fixed-top has-shadow px-3" aria-label="main navigation">
@@ -10,8 +12,18 @@
           alt="Navbar Logo"
           width="112" height="28">
     </a>
+
+    <a 
+      role="button" class="navbar-burger"
+      on:click={() => is_active= !is_active }
+    >
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
   </div>
-  <div id="navbarBasicExample" class="navbar-menu">
+
+  <div class="navbar-menu {is_active ? "is-active" : ""}">
     <div class="navbar-start"></div>
     <div class="navbar-end">
       {#if location.pathname === "/"}
